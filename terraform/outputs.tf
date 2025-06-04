bc0770f4e219be8872d712e23ee4da30fbdf73ea
Subhajit <86037155+Subhajit112@users.noreply.github.com>
2025-06-05 00:32:28 +0530

    Update outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
